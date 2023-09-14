e47820ded3dd5270a7e6df31b0f148372584201a
Mohamed Ghoneim <maghoneim@2p.com.sa>
2023-09-14 11:52:15 +0300

    draggable

---
+++ lib/core/draggable_floating_action_button.dart
@@ -0,0 +1,113 @@
+// ignore_for_file: cast_nullable_to_non_nullable, use_named_constants
+
+import 'package:flutter/material.dart';
+import 'my_app.dart';
+
+class DraggableFloatingActionButton extends StatefulWidget {
+  final Widget child;
+  final Offset initialOffset;
+  final VoidCallback onPressed;
+
+  const DraggableFloatingActionButton({
+    super.key,
+    required this.child,
+    required this.initialOffset,
+    required this.onPressed,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _DraggableFloatingActionButtonState();
+}
+
+class _DraggableFloatingActionButtonState
+    extends State<DraggableFloatingActionButton> {
+  final GlobalKey _key = GlobalKey();
+
+  bool _isDragging = false;
+  late Offset _offset;
+  late Offset _minOffset;
+  late Offset _maxOffset;
+
+  @override
+  void initState() {
+    super.initState();
+    _offset = widget.initialOffset;
+
+    WidgetsBinding.instance.addPostFrameCallback(_setBoundary);
+  }
+
+  void _setBoundary(_) {
+    final RenderBox parentRenderBox =
+        parentKey.currentContext?.findRenderObject() as RenderBox;
+    final RenderBox renderBox =
+        _key.currentContext?.findRenderObject() as RenderBox;
+
+    try {
+      final Size parentSize = parentRenderBox.size;
+      final Size size = renderBox.size;
+
+      setState(() {
+        _minOffset = const Offset(0, 0);
+        _maxOffset = Offset(
+          parentSize.width - size.width,
+          parentSize.height - size.height,
+        );
+      });
+    } catch (e) {
+      debugPrint('catch: $e');
+    }
+  }
+
+  void _updatePosition(PointerMoveEvent pointerMoveEvent) {
+    double newOffsetX = _offset.dx + pointerMoveEvent.delta.dx;
+    double newOffsetY = _offset.dy + pointerMoveEvent.delta.dy;
+
+    if (newOffsetX < _minOffset.dx) {
+      newOffsetX = _minOffset.dx;
+    } else if (newOffsetX > _maxOffset.dx) {
+      newOffsetX = _maxOffset.dx;
+    }
+
+    if (newOffsetY < _minOffset.dy) {
+      newOffsetY = _minOffset.dy;
+    } else if (newOffsetY > _maxOffset.dy) {
+      newOffsetY = _maxOffset.dy;
+    }
+
+    setState(() {
+      _offset = Offset(newOffsetX, newOffsetY);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: _offset.dx,
+      top: _offset.dy,
+      child: Listener(
+        onPointerMove: (PointerMoveEvent pointerMoveEvent) {
+          _updatePosition(pointerMoveEvent);
+
+          setState(() {
+            _isDragging = true;
+          });
+        },
+        onPointerUp: (PointerUpEvent pointerUpEvent) {
+          debugPrint('onPointerUp');
+
+          if (_isDragging) {
+            setState(() {
+              _isDragging = false;
+            });
+          } else {
+            widget.onPressed();
+          }
+        },
+        child: Container(
+          key: _key,
+          child: widget.child,
+        ),
+      ),
+    );
+  }
+}
